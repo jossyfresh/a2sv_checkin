dbb0e647a21a9ac586ea850a10532faa3f9e5f73
jossyfresh <jossyfresh2002@gmail.com>
2024-08-19 14:40:44 +0300

    bot username change to as2v bounce

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from telethon import TelegramClient, events
+from telethon.tl.functions.messages import GetBotCallbackAnswerRequest
+from telethon.errors import SessionPasswordNeededError, BotResponseTimeoutError
+import asyncio
 
 # Replace these with your API ID and API hash
 api_id = '28592154'
@@ -13,29 +16,52 @@ async def checkin():
     # Connect to Telegram servers
     await client.start(phone_number)
 
-    # Send a message to a specific user or chat
-    # Replace 'user_id_or_username' with the target user ID or username
+    # Send a message to the bot to initiate the check-in process
     await client.send_message('@A2SVBouncerbot', '/checkin')
+
+
+    @client.on(events.NewMessage(from_users='@A2SVBouncerbot'))
+    async def handler(event):
+        message = event.message
+        print(message)
+        if message.buttons:
+            # Find the button with "AASTU In Person" text and click it
+            for row in message.buttons:
+                for button in row:
+                    if button.text == 'AASTU In Person':
+                        await client(GetBotCallbackAnswerRequest(
+                            peer=message.peer_id,
+                            msg_id=message.id,
+                            data=button.data
+                        ))
+                        print("Clicked 'AASTU In Person'")
+                        return
+        # Run the event handler
+
 async def checkout():
+
     # Connect to Telegram servers
     await client.start(phone_number)
 
-    # Send a message to a specific user or chat
-    # Replace 'user_id_or_username' with the target user ID or username
+    # Send a message to the bot to initiate the checkout process
     await client.send_message('@A2SVBouncerbot', '/checkout')
 
-    # You can also add event handlers here to respond to messages or other events
+# Initialize FastAPI
 app = FastAPI()
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
 @app.get("/checkin")
 async def check_in():
     await checkin()
     return {"message": "Checked in!"}
+
 @app.get("/checkout")
 async def check_out():
     await checkout()
